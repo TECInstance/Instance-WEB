ab2c2f8f972fb75b7e2ab01821116cba6c07b6d6
nikhunter <niklas@micheelsen.com>
2015-12-01 08:50:20 +0100

    test

---
+++ Instance Web/Pages/Front Page.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Front Page.aspx.cs" Inherits="Instance_Web.Pages.Front_Page" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Instance</title>
+</head>
+<center>
+    <form id="form1" runat="server">
+        <br />
+        <br />
+        <br />
+        Instance - Registration site.<br />
+        <br />
+        Please enter a username and password:<br />
+        <br />
+        <div style="height: 481px; width: 1058px">
+    
+            Username:<asp:TextBox ID="username" runat="server"></asp:TextBox>
+            <br/>
+            Password: <asp:TextBox ID="password" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button runat="server" Text="Create account" ID="create_account" Height="26px" OnClick="create_account_Click"></asp:Button>
+    </div>
+    </form>
+</center>
+</html>
---
+++ Instance Web/Pages/Front Page.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Instance_Web.Pages {
+    public partial class Front_Page : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+
+        protected void create_account_Click(object sender, EventArgs e) {
+            string connectionString = @"Data Source=72HKJ12\PULSE;Initial Catalog=Instance;Integrated Security=SSPI";
+
+            using (SqlConnection con = new SqlConnection(connectionString)) {
+
+                SqlCommand command = new SqlCommand("INSERT INTO logins(usernames,passwords) values(@username,@password)", con);
+                command.Parameters.AddWithValue("@username", username.Text);
+                command.Parameters.AddWithValue("@password", HashThis(password.Text));
+                con.Open();
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private string HashThis(string password) {
+                using (SHA1Managed sha1 = new SHA1Managed())
+                {
+                    var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(password));
+                    var sb = new StringBuilder(hash.Length * 2);
+
+                    foreach (byte b in hash)
+                    {
+                        // can be "x2" if you want lowercase
+                        sb.Append(b.ToString("X2"));
+                    }
+
+                    return sb.ToString();
+            }
+        }
+    }
+}
