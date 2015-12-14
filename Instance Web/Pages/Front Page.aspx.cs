5904f99303106ae51bece007c4b36fc244ea94a3
nikhunter <niklas@micheelsen.com>
2015-12-14 10:41:34 +0100

    Final commit before turn-in

--- Instance Web/Pages/Front Page.aspx.cs
+++ Instance Web/Pages/Front Page.aspx.cs
@@ -1,80 +1,97 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace Instance_Web.Pages {
     public partial class FrontPage : Page {
         protected void Page_Load(object sender, EventArgs e) {
-
         }
 
+        // Extracts fields and inserts to SQL table
         protected void create_account_Click(object sender, EventArgs e) {
-            string connectionString = @"Data Source=80.198.77.171,1337; Initial Catalog=Instance; User Id = InstanceLogin; Password = password";
+            // Connection string for SQL connection
+            var connectionString = @"Data Source=80.198.77.171,1337; Initial Catalog=Instance; User Id = InstanceLogin; Password = password";
 
+            // If usernames exists, shows error
             if (DoesExist(username.Text)) {
                 MessageBox("Username is taken.");
             }
             else {
                 try {
-                    using (SqlConnection con = new SqlConnection(connectionString))
-                    {
-                        SqlCommand command = new SqlCommand("INSERT INTO logins values(@username,@password,@title,default)", con);
+                    // SQL Connection
+                    using (var con = new SqlConnection(connectionString)) {
+                        // SQL Command, inserts username, password, title and online status via connection named con
+                        var command = new SqlCommand("INSERT INTO logins values(@username,@password,@title,default)", con);
                         command.Parameters.AddWithValue("@username", username.Text);
                         command.Parameters.AddWithValue("@password", HashThis(password.Text));
                         command.Parameters.AddWithValue("@title", title.Text);
                         con.Open();
 
+                        // Executes SQL Command named command
                         command.ExecuteNonQuery();
                     }
                 }
+                    // If connection cannot be established, show error
                 catch (Exception) {
                     MessageBox("Connection refused");
                 }
             }
 
+            // Empties all fields
             username.Text = null;
             title.Text = null;
             password.Text = null;
+
+            // Informs user that account is created
             MessageBox("Account successfully created.");
         }
 
-        private string HashThis(string pass) {
-            using (SHA1Managed sha1 = new SHA1Managed()) {
+
+        // Hashes Password and returns hash value
+        private static string HashThis(string pass) {
+            // Generates SHA1
+            using (var sha1 = new SHA1Managed()) {
                 var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(pass));
                 var sb = new StringBuilder(hash.Length*2);
 
-                foreach (byte b in hash) {
+                foreach (var b in hash) {
                     sb.Append(b.ToString("X2"));
                 }
 
+                // Returns hash value
                 return sb.ToString();
             }
         }
 
-        protected void MessageBox(string _str) {
-            Page.ClientScript.RegisterStartupScript(GetType(), "scriptkey", "<script>alert('"+ _str +"');</script>");
+        // Function for displaying message boxes in JS
+        protected void MessageBox(string str) {
+            Page.ClientScript.RegisterStartupScript(GetType(), "scriptkey", "<script>alert('" + str + "');</script>");
         }
 
-        protected bool DoesExist (string str) {
 
-            string connectionString = @"Data Source=80.198.77.171,1337; Initial Catalog=Instance; User Id = InstanceLogin; Password = password";
+        // Returns all usernames
+        protected bool DoesExist(string str) {
+            // Connection string for SQL connection
+            var connectionString = @"Data Source=80.198.77.171,1337; Initial Catalog=Instance; User Id = InstanceLogin; Password = password";
 
-            using (SqlConnection con = new SqlConnection(connectionString)) {
-                DataTable dt = new DataTable();
+            // Connection establishment
+            using (var con = new SqlConnection(connectionString)) {
+                // Creates datatable
+                var dt = new DataTable();
                 con.Open();
 
+                // SQL statement
                 var command = new SqlCommand("select * from logins", con);
-                SqlDataReader dr = command.ExecuteReader();
+
+                // Creates datareader from SQL command
+                var dr = command.ExecuteReader();
                 dt.Load(dr);
 
+                // Functions as foreach using LINQ expression
                 return dt.Rows.Cast<DataRow>().Any(row => row.Field<string>("usernames") == str);
             }
         }
